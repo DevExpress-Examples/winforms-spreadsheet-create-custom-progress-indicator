c4371a6034058bccb57e0823007dde870e0821f3
DevExpressExampleBot <fakeEmail>
2021-11-09 20:39:30 +0300

    autogenerate VB

--- VB/SpreadsheetProgressSample/WaitForm1.vb
+++ VB/SpreadsheetProgressSample/WaitForm1.vb
@@ -1,42 +1,41 @@
-﻿Imports System
+Imports System
 Imports System.Threading
 Imports DevExpress.XtraWaitForm
 
 Namespace SpreadsheetProgressSample
-	Partial Public Class WaitForm1
-		Inherits WaitForm
 
-		Private cancellationTokenSource As CancellationTokenSource
+    Public Partial Class WaitForm1
+        Inherits WaitForm
 
-		Public Sub New()
-			InitializeComponent()
-			progressPanel1.AutoSize = True
-		End Sub
+        Private cancellationTokenSource As CancellationTokenSource
 
-		Public Overrides Sub SetCaption(ByVal caption As String)
-			MyBase.SetCaption(caption)
-			progressPanel1.Caption = caption
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            progressPanel1.AutoSize = True
+        End Sub
 
-		Public Overrides Sub SetDescription(ByVal description As String)
-			MyBase.SetDescription(description)
-			progressPanel1.Description = description
-		End Sub
+        Public Overrides Sub SetCaption(ByVal caption As String)
+            MyBase.SetCaption(caption)
+            progressPanel1.Caption = caption
+        End Sub
 
-		Public Overrides Sub ProcessCommand(ByVal cmd As System.Enum, ByVal arg As Object)
-			MyBase.ProcessCommand(cmd, arg)
-			Dim command As WaitFormCommand = CType(cmd, WaitFormCommand)
-			If command = WaitFormCommand.SetCancellationTokenSource Then
-				cancellationTokenSource = DirectCast(arg, CancellationTokenSource)
-			End If
-		End Sub
+        Public Overrides Sub SetDescription(ByVal description As String)
+            MyBase.SetDescription(description)
+            progressPanel1.Description = description
+        End Sub
 
-		Public Enum WaitFormCommand
-			SetCancellationTokenSource
-		End Enum
+        Public Overrides Sub ProcessCommand(ByVal cmd As [Enum], ByVal arg As Object)
+            MyBase.ProcessCommand(cmd, arg)
+            Dim command As WaitFormCommand = CType(cmd, WaitFormCommand)
+            If command = WaitFormCommand.SetCancellationTokenSource Then cancellationTokenSource = CType(arg, CancellationTokenSource)
+        End Sub
 
-		Private Sub Cancel_Click(ByVal sender As Object, ByVal e As EventArgs) Handles hyperlinkLabelControl1.Click
-			cancellationTokenSource?.Cancel()
-		End Sub
-	End Class
+        Public Enum WaitFormCommand
+            SetCancellationTokenSource
+        End Enum
+
+        Private Sub Cancel_Click(ByVal sender As Object, ByVal e As EventArgs)
+            cancellationTokenSource?.Cancel()
+        End Sub
+    End Class
 End Namespace
